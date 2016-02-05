f46bd1c8bb8d006578e03cd2a4b901adaf2ead71
Bastien Chevallier <cheval_b@etna-alternance.net>
2016-02-05 17:10:37 +0100

    fix master

--- index.android.js
+++ index.android.js
@@ -35,16 +35,11 @@ class Sweetset extends Component {
         break;
       }
   }
-  changePage(page) {
+  changePage = (e, page) => {
     this.setState({
       page: page
     });
-  }
+  };
 }
-
-<<<<<<< HEAD
-const styles = require('./css.js');
-=======
->>>>>>> 89cc10b7953e69239ab04ec79cb67f0e8ae2f9a0
 
 AppRegistry.registerComponent('Sweetset', () => Sweetset);
