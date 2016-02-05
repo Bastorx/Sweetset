44c301eb63a9af80e8097f7f8c57bcb143f00d8d
Johnny Yoeurng <yoeurng.johnny@gmail.com>
2016-02-05 10:26:02 +0100

    test02

--- paiement.js
+++ paiement.js
@@ -3,7 +3,6 @@ import React, {
   AppRegistry,
   Component,
   StyleSheet,
-  TouchableNativeFeedback,
   Text,
   View
 } from 'react-native';
@@ -29,18 +28,6 @@ class Sweetset extends Component {
         </Text>
       </View>
     );
-
-    renderButton: function() {
-      return (
-        <TouchableNativeFeedback
-            onPress={this._onPressButton}
-            background={TouchableNativeFeedback.SelectableBackground()}>
-          <View style={{width: 150, height: 100, backgroundColor: 'red'}}>
-            <Text style={{margin: 30}}>Button</Text>
-          </View>
-        </TouchableNativeFeedback>
-      );
-    },
   }
 }
 
