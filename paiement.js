1a8c728fa8238c92159f33c111c171118266012f
Johnny Yoeurng <yoeurng.johnny@gmail.com>
2016-02-05 14:25:53 +0100

    test10

--- paiement.js
+++ paiement.js
@@ -15,12 +15,18 @@ class Paiement extends Component {
         <Text>Payment</Text>
         <Text>Nom titulaire :</Text>
         <TextInput
-           style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-           placeholder="Password"
-           placeholdertextcolor="blue"
-           onChangeText={(text) => this.setState({text})}
-           value={this.state.text}
-        />
+          style={{
+            aligneItems: "center",
+            alignSelf: "center",
+            justifyContent: "center",
+            top: -10, height: 50, width: 300, padding: 60,
+            borderWidth: 1, borderColor: "#fff", color: "#fff",
+            backgroundColor: "transparent"
+          }}
+          placeholder="Password"
+          placeholderTextColor="blue"
+          onChangeText={(text) => this.setState({c: text})}/>
+          
         <Text>Numéro de carte :</Text>
         <Text>GCV :</Text>
         <View style={styles.button}>
