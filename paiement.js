632757685d15658851678e43189f1fa545541bc1
Johnny Yoeurng <yoeurng.johnny@gmail.com>
2016-02-05 09:43:59 +0100

    rajout de bouton

--- paiement.js
+++ paiement.js
@@ -28,6 +28,18 @@ class Sweetset extends Component {
         </Text>
       </View>
     );
+
+    renderButton: function() {
+      return (
+        <TouchableNativeFeedback
+            onPress={this._onPressButton}
+            background={TouchableNativeFeedback.SelectableBackground()}>
+          <View style={{width: 150, height: 100, backgroundColor: 'red'}}>
+            <Text style={{margin: 30}}>Button</Text>
+          </View>
+        </TouchableNativeFeedback>
+      );
+    },
   }
 }
 
