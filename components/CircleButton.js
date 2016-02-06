c9b1bd1ce1dc9577c8bded44f0f2235514d10e77
Bastien Chevallier <cheval_b@etna-alternance.net>
2016-02-06 02:39:19 +0100

    accueil

---
+++ components/CircleButton.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import React, {
+  Component,
+  View,
+  Text
+} from 'react-native';
+
+class Title extends Component {
+  render() {
+    const bgColor = this.props.bgColor || "rgba(128, 128, 128, 0.8)";
+    const fontColor = this.props.color || "white";
+    return (
+      <View style={{marginTop: 20, marginRight: 15, width: 100, height: 100, borderRadius: 1000, backgroundColor: bgColor}}>
+        <Text style={{color: fontColor, marginTop: 40, textAlign:"center", fontWeight: "500", fontSize: 14}}>{this.props.text}</Text>
+      </View>
+    );
+  }
+}
+
+export default Title;
