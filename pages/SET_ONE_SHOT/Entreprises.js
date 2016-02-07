33105a2bca0f51fba8ec6d05105ec8a4cbbb1c44
Sinthuvan VIGNESWARAN <Sinthuvan VIGNESWARAN>
2016-02-07 23:08:56 +0100

    Entreprise tab et \n

--- pages/SET_ONE_SHOT/Entreprises.js
+++ pages/SET_ONE_SHOT/Entreprises.js
@@ -16,10 +16,10 @@ class Entreprises extends Component {
     return (
         <View>
         <Text>
-        Sweetset permet aux entreprises d'offrir des sets originaux aux collaborateurs lors d'événements importants (Noël, challenge remporté, ...). Vous pourrez ainsi faire du sur-mesure pour offrir un moment de plaisir aux personnes de votre entreprise mais également surprendre vos clients VIP, amateurs de vin, champagne ou whisky en les livrant le jour même à l'adresse que vous aurez choisi.
+        {"\t"}Sweetset permet aux entreprises d'offrir des sets originaux aux collaborateurs lors d'événements importants (Noël, challenge remporté, ...). Vous pourrez ainsi faire du sur-mesure pour offrir un moment de plaisir aux personnes de votre entreprise mais également surprendre vos clients VIP, amateurs de vin, champagne ou whisky en les livrant le jour même à l'adresse que vous aurez choisi.
 Nous vous proposons ce service afin de vous soulager des divers contraintes, nous sommes à votre service et vous pourrez compter sur nous ainsi que sur la qualité de nos produits et de notre service de livraison express.
-Pour toute demande d'informations ou de devis, n'hésitez plus, contactez-nous : headoffice@sweetset.paris 
-A bientôt,
+Pour toute demande d'informations ou de devis, n'hésitez plus, contactez-nous : headoffice@sweetset.paris{"\n"} 
+A bientôt,{"\n"}
 L'équipe Sweetset.
         </Text>
         </View>
